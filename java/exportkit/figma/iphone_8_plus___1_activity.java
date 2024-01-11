22cbf31b1bf91291daccba98a06e49a12d33b0df
Yuri89 <69095138+Yuri89@users.noreply.github.com>
2024-01-11 17:34:59 -0300

    Add files via upload

---
+++ java/exportkit/figma/iphone_8_plus___1_activity.java
@@ -0,0 +1,80 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		iphone_8_plus___1
+	 *	@date 		Thursday 11th of January 2024 08:27:08 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class iphone_8_plus___1_activity extends Activity {
+
+	
+	private View _bg__iphone_8_plus___1_ek2;
+	private ImageView ellipse_1;
+	private View _bg__login_ek1;
+	private View rectangle_1;
+	private View rectangle_2;
+	private View rectangle_3;
+	private TextView ___login__;
+	private View _bg__mdi_user_ek1;
+	private ImageView vector;
+	private View _bg__material_symbols_lock_ek1;
+	private ImageView vector_ek1;
+	private View _bg__bot_o_entrar_ek1;
+	private View rectangle_4;
+	private TextView entrar;
+	private TextView esqueceu_a_senha_;
+	private TextView senha;
+	private TextView usuarios;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.iphone_8_plus___1);
+
+		
+		_bg__iphone_8_plus___1_ek2 = (View) findViewById(R.id._bg__iphone_8_plus___1_ek2);
+		ellipse_1 = (ImageView) findViewById(R.id.ellipse_1);
+		_bg__login_ek1 = (View) findViewById(R.id._bg__login_ek1);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		___login__ = (TextView) findViewById(R.id.___login__);
+		_bg__mdi_user_ek1 = (View) findViewById(R.id._bg__mdi_user_ek1);
+		vector = (ImageView) findViewById(R.id.vector);
+		_bg__material_symbols_lock_ek1 = (View) findViewById(R.id._bg__material_symbols_lock_ek1);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		_bg__bot_o_entrar_ek1 = (View) findViewById(R.id._bg__bot_o_entrar_ek1);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		entrar = (TextView) findViewById(R.id.entrar);
+		esqueceu_a_senha_ = (TextView) findViewById(R.id.esqueceu_a_senha_);
+		senha = (TextView) findViewById(R.id.senha);
+		usuarios = (TextView) findViewById(R.id.usuarios);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
